69ff74d9f686e89e859a297ef0d572c1025fe831
Eason050633 <ke73219@gmail.com>
2026-07-02 22:16:20 +0800

    上線離線按鈕製作

--- UDPChat/Form1.cs
+++ UDPChat/Form1.cs
@@ -22,6 +22,12 @@ namespace UDPChat
         }
         UdpClient udpClient;
         Thread t;
+        String MyName;
+        ArrayList ips = new ArrayList();//上線人員的IP列表
+        const short port = 2019;//UDP通訊埠
+        string BC=IPAddress.Broadcast.ToString();//廣播位址
+
+
         private string MyIP()
         {
             string myIP=Dns.GetHostName();
@@ -37,9 +43,18 @@ namespace UDPChat
         }
         private void Form1_Load(object sender, EventArgs e)
         {
+            Control.CheckForIllegalCrossThreadCalls = false;
             this.Text=" "+MyIP();
         }
-
+        private void Send(string ToIP,string msg,string ToWhom)
+        {
+            //我是誰+IP+訊息+發給誰
+            string a = MyName+" "+MyIP()+":"+msg+" "+ToWhom;
+            byte[] b = Encoding.Default.GetBytes(a);
+            UdpClient udpClient = new UdpClient();
+            udpClient.Send(b, b.Length);
+        }
+        
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             try
